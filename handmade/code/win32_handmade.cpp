e52a5d0362cc0701652bef236c6c26e8d1e58a21
brandonce <brandonce@gmail.com>
2018-05-20 17:38:31 +0900

    Day 002
    A bunch of Windows calls...
    
    build
    devenv path/to/file.exe
    F11 and shift+F5

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -4,12 +4,125 @@ header comment
 
 #include <windows.h>
 
-int CALLBACK WinMain(
-   HINSTANCE hInstance,
-   HINSTANCE hPrevInstance,
-   LPSTR     lpCmdLine,
-   int       nCmdShow)
+LRESULT CALLBACK 
+MainWindowsCallback(HWND   Window,
+                    UINT   Message,
+                    WPARAM WParam,
+                    LPARAM LParam)
 {
-	MessageBoxA (0, "asdf", "qwer", MB_OK | MB_ICONINFORMATION);
-	return(0);
+    LRESULT Result = 0;
+
+    switch (Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_PAINT:
+        {
+            OutputDebugStringA("WM_PAINT\n");
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            static DWORD Operation = WHITENESS;
+            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+            if (Operation == WHITENESS)
+            {
+                Operation = BLACKNESS;
+            }
+            else 
+            {
+                Operation = WHITENESS;
+            }
+            EndPaint(Window, &Paint);
+
+        } break;
+
+        default:
+        {
+            //OutputDebugStringA("default\n");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return(Result);
+}
+
+
+int CALLBACK WinMain(
+   HINSTANCE Instance,
+   HINSTANCE PrevInstance,
+   LPSTR     CommandLine,
+   int       ShowCode)
+{
+    WNDCLASS WindowClass = {};
+
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowsCallback;
+    WindowClass.hInstance = Instance;
+    //WindowClass.hIcon; //TODO later
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+    
+    if (RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = 
+            CreateWindowEx(
+                0,
+                WindowClass.lpszClassName,
+                "Handmade Hero",
+                WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                0,
+                0,
+                Instance,
+                0);
+        if (WindowHandle)
+        {
+            MSG Message;
+            for (;;)
+            {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if(MessageResult > 0)
+                {
+                    TranslateMessage(&Message); // ignore for now
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            //TODO logging
+        }
+    }
+    else 
+    {
+        // TODO if it fails
+    }
+    return(0);
 }
